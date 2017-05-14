a208ae50a2201705357327040bef49fa8405cc36
Kenneth Aalberg <kenneaal@gmail.com>
2017-05-15 01:52:38 +0200

    Experimental cheaty inserts on new systems.

--- updatedb.py
+++ updatedb.py
@@ -1,4 +1,4 @@
-import os, sys, transaction
+import os, sys, transaction, requests
 
 from odo import odo, dshape
 
@@ -7,7 +7,7 @@ from pyramid.paster import (
     get_appsettings,
     setup_logging,
 )
-from sqlalchemy import engine_from_config
+from sqlalchemy import engine_from_config, sql, orm, schema
 from zope.sqlalchemy import mark_changed
 
 from eddb_jsonapi.mymodels import (
@@ -51,7 +51,7 @@ def main(argv=sys.argv):
     print("Saved factions.json. Dropping and updating.")
     DBSession.execute("DELETE FROM factions")
     mark_changed(DBSession())
-    DBSession.commit()
+    transaction.commit()
     url = str(engine.url) + "::" + Faction.__tablename__
     ds = dshape("var *{  id: ?int64,  name: ?string,  updated_at: ?int64,  government_id: ?int64,  "
                 "government: ?string,  allegiance_id: ?int64,  allegiance: ?string,  "
@@ -60,7 +60,7 @@ def main(argv=sys.argv):
     t = odo('jsonlines://factions.json', url, dshape=ds)
     print("Done!")
     mark_changed(DBSession())
-    DBSession.commit()
+    transaction.commit()
 
     #
     # Populated systems
@@ -74,7 +74,7 @@ def main(argv=sys.argv):
     print("Saved systems_populated.json. Dropping and updating.")
     DBSession.execute("DELETE FROM populated_systems")
     mark_changed(DBSession())
-    DBSession.commit()
+    transaction.commit()
     url = str(engine.url) + "::" + PopulatedSystem.__tablename__
     ds = dshape("var *{  id: ?int64,  edsm_id: ?int64,  name: ?string,  x: ?float64,  y: ?float64,  "
                 "z: ?float64,  population: ?int64,  is_populated: ?bool,  government_id: ?int64,  "
@@ -95,7 +95,28 @@ def main(argv=sys.argv):
     #
     # TODO: Update systems
     #
-
+    print("Downloading systems_recently.csv from EDDB.io...")
+    r = requests.get("https://eddb.io/archive/v5/systems_recently.csv", stream=True)
+    with open('systems_recently.csv', 'wb') as f:
+        for chunk in r.iter_content(chunk_size=4096):
+            if chunk:
+                f.write(chunk)
+    print("Saved systems_recently.csv. Creating temporary table and importing.")
+    DBSession.execute("CREATE TEMP TABLE systems_tmp ON COMMIT DROP AS SELECT * FROM systems WITH NO DATA")
+    url = str(engine.url) + "::systems_tmp"
+    ds = dshape("var *{  id: ?int64,  edsm_id: ?int64,  name: ?string,  x: ?float64,  y: ?float64,  "
+                "z: ?float64,  population: ?int64,  is_populated: ?bool,  government_id: ?int64,  "
+                "government: ?string,  allegiance_id: ?int64,  allegiance: ?string,  "
+                "state_id: ?int64,  state: ?string,  security_id: ?float64,  security: ?string,  "
+                "primary_economy_id: ?float64,  primary_economy: ?string,  power: ?string,  "
+                "power_state: ?string,  power_state_id: ?string,  needs_permit: ?bool,  "
+                "updated_at: ?int64,  simbad_ref: ?string,  controlling_minor_faction_id: ?string,  "
+                "controlling_minor_faction: ?string,  reserve_type_id: ?float64,  reserve_type: ?string  }")
+    t = odo('systems_recently.csv', url, dshape=ds)
+    print("Updating systems...")
+    DBSession.execute("INSERT INTO systems SELECT * FROM systems_tmp ON CONFLICT DO UPDATE")
+    mark_changed(DBSession())
+    transaction.commit()
     #
     # TODO: Update stations
     #
