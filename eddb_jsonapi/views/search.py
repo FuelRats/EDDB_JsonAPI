b45f30026ed5019a426c1fc4379da2417936f6c2
Kenneth Aalberg <kenneaal@gmail.com>
2018-04-08 10:19:51 +0200

    Make Postgres slightly happier...

--- eddb_jsonapi/views/search.py
+++ eddb_jsonapi/views/search.py
@@ -44,13 +44,13 @@ def search(request):
         if 'name' not in request.params:
             return {'meta': {'error': 'No name specified.'}}
         if searchtype == 'lev':
-            sql = text('SELECT *, lev_distance(name, "' + name +'") AS similarity FROM systems ' +
-                       ' WHERE name \% "'+name+'" ORDER BY similarity DESC')
+            sql = text('SELECT *, lev_distance(name, \'' + name + '\') AS similarity FROM systems ' +
+                       ' WHERE name \% \''+name+'\' ORDER BY similarity DESC')
         if searchtype == 'soundex':
             sql = text('SELECT *, similarity(name, "' + name +
                        '") AS similarity FROM systems WHERE soundex(name) ' +
-                       '= soundex("' + name + '") ORDER BY similarity(name, "' +
-                       name + '")')
+                       '= soundex(\'' + name + '\') ORDER BY similarity(name, \'' +
+                       name + '\')')
         result = DBSession.execute(sql)
         candidates = []
         ids = []
