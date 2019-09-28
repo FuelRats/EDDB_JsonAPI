6fd1e297aa819c4cd727c3286a6dc27e54cdb6f4
Kenneth Aalberg <kenneaal@gmail.com>
2019-09-28 16:05:11 +0200

    Create custom request factory for XHR requests.

--- eddb_jsonapi/__init__.py
+++ eddb_jsonapi/__init__.py
@@ -1,10 +1,26 @@
 from pyramid.config import Configurator
+from pyramid.request import Request
+from pyramid.response import Response
 
 from sqlalchemy import engine_from_config
 from . import edsmmodels
 import pyramid_jsonapi
 from pyramid_beaker import set_cache_regions_from_settings
 from pyramid_beaker import session_factory_from_settings
+
+
+def request_factory(environ):
+    request = Request(environ)
+    if request.is_xhr:
+        request.response = Response()
+        request.response.headerlist = []
+        request.response.headerlist.extend(
+            (
+                ('Access-Control-Allow-Origin', '*'),
+                ('Content-Type', 'application/json')
+            )
+        )
+    return request
 
 
 def main(global_config, **settings):
@@ -24,6 +40,7 @@ def main(global_config, **settings):
     config.add_route('home', '/')
     config.add_route('nearest', '/nearest')
     config.add_route('search', '/search')
+    config.set_request_factory(request_factory)
     pj = pyramid_jsonapi.PyramidJSONAPI(config, edsmmodels, lambda view: edsmmodels.DBSession)
     pj.create_jsonapi_using_magic_and_pixie_dust()
 
