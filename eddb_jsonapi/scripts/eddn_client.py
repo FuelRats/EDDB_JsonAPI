73afab774fc607e480e1c56fded703c275d11bbb
Kenneth Aalberg <kenneaal@gmail.com>
2020-01-29 13:24:19 +0100

    Only trust established sources of data.

--- eddb_jsonapi/scripts/eddn_client.py
+++ eddb_jsonapi/scripts/eddn_client.py
@@ -16,6 +16,7 @@ from pyramid.paster import (
 
 from pyramid.scripts.common import parse_vars
 from sqlalchemy import func
+from sqlalchemy.exc import DataError
 
 from eddb_jsonapi.models import (
     get_engine,
@@ -30,6 +31,17 @@ __relayEDDN = 'tcp://eddn.edcd.io:9500'
 __timeoutEDDN = 600000
 __scoopable = ['K', 'G', 'B', 'F', 'O', 'A', 'M']
 
+__authorisedSoftwares   = [
+    "E:D Market Connector",
+    "EDDiscovery",
+    "EDDI",
+    "EDCE",
+    "ED-TD.SPACE",
+    "EliteOCR",
+    "Maddavo's Market Share",
+    "RegulatedNoise",
+    "RegulatedNoise__DJ"
+]
 
 def usage(argv):
     cmd = os.path.basename(argv[0])
@@ -74,74 +86,80 @@ def main(argv=sys.argv):
 
                 __message = zlib.decompress(__message)
                 __json = simplejson.loads(__message)
-                if time.time() > (starttime + 3600 * 24):
-                    try:
-                        startot = session.query(func.count(Star.id64)).scalar()
-                        systot = session.query(func.count(System.id64)).scalar()
-                        proxy.command("botserv", "Absolver", f"say #ratchat [\x0315SAPI\x03] Daily report: "
-                                                             f"{'{:,}'.format(messages)} messages processed"
-                                                             f", {'{:,}'.format(syscount)} new systems,"
-                                                             f"  {'{:,}'.format(starcount)} new stars."
-                                                             f" DB contains {'{:,}'.format(startot)} stars "
-                                                             f"and {'{:,}'.format(systot)} systems.")
-                        messages = 0
-                        syscount = 0
-                        starcount = 0
-                        starttime = time.time()
-                    except TimeoutError:
-                        print("XMLRPC call failed due to timeout, retrying in 60 seconds.")
-                        starttime = starttime + 60
-                data = __json['message']
-                messages = messages + 1
-                if 'event' in data:
-                    if data['event'] == 'FSDJump':
-                        id64 = data['SystemAddress']
-                        res = session.query(System.id64).filter(System.id64 == id64).scalar() or False
-                        if not res:
-                            syscount = syscount + 1
-                            newsys = System(id64=data['SystemAddress'], name=data['StarSystem'],
-                                            coords=data['StarPos'], date=data['timestamp'])
-                            session.add(newsys)
-                            transaction.commit()
-                    if data['event'] == 'Scan':
-                        bodyid = data['SystemAddress'] + (data['BodyID'] << 55)
-                        if 'AbsoluteMagnitude' in data:
-                            res = session.query(Star.id64).filter(Star.id64 == bodyid).scalar() or False
+                if __json['header']['softwareName'] in __authorisedSoftwares:
+
+                    if time.time() > (starttime + 3600 * 24):
+                        try:
+                            startot = session.query(func.count(Star.id64)).scalar()
+                            systot = session.query(func.count(System.id64)).scalar()
+                            proxy.command("botserv", "Absolver", f"say #ratchat [\x0315SAPI\x03] Daily report: "
+                                                                 f"{'{:,}'.format(messages)} messages processed"
+                                                                 f", {'{:,}'.format(syscount)} new systems,"
+                                                                 f"  {'{:,}'.format(starcount)} new stars."
+                                                                 f" DB contains {'{:,}'.format(startot)} stars "
+                                                                 f"and {'{:,}'.format(systot)} systems.")
+                            messages = 0
+                            syscount = 0
+                            starcount = 0
+                            starttime = time.time()
+                        except TimeoutError:
+                            print("XMLRPC call failed due to timeout, retrying in 60 seconds.")
+                            starttime = starttime + 60
+                    data = __json['message']
+                    messages = messages + 1
+                    if 'event' in data:
+                        if data['event'] == 'FSDJump':
+                            id64 = data['SystemAddress']
+                            res = session.query(System.id64).filter(System.id64 == id64).scalar() or False
                             if not res:
-                                starcount = starcount + 1
-                                newstar = Star(id64=bodyid, bodyId=data['BodyID'], name=data['BodyName'],
-                                               age=data['Age_MY'], axialTilt=data['AxialTilt'],
-                                               orbitalEccentricity=data['Eccentricity']
-                                               if 'Eccentricity' in data else None,
-                                               orbitalInclination=data['OrbitalInclination']
-                                               if 'OrbitalInclination' in data else None,
-                                               orbitalPeriod=data['OrbitalPeriod']
-                                               if 'OrbitalPeriod' in data else None,
-                                               parents=data['Parents']
-                                               if 'Parents' in data else None,
-                                               argOfPeriapsis=data['Periapsis']
-                                               if 'Periapsis' in data else None,
-                                               belts=data['Rings'] if 'Rings' in data else None,
-                                               semiMajorAxis=data['SemiMajorAxis']
-                                               if 'SemiMajorAxis' in data else None,
-                                               systemName=data['StarSystem'],
-                                               distanceToArrival=data['DistanceFromArrivalLS'],
-                                               luminosity=data['Luminosity'], solarRadius=data['Radius'],
-                                               rotationalPeriod=data['RotationPeriod'], type=data['StarType'],
-                                               solarMasses=data['StellarMass'],
-                                               subType=data['Subclass'] if 'Subclass' in data else None,
-                                               surfaceTemperature=data['SurfaceTemperature'],
-                                               isScoopable=True if data['StarType'] in __scoopable else False,
-                                               isMainStar=True if data['BodyID'] == 0 else False,
-                                               updateTime=data['timestamp'])
-                                session.add(newstar)
-                                print("Added new star.")
-                                transaction.commit()
+                                syscount = syscount + 1
+                                newsys = System(id64=data['SystemAddress'], name=data['StarSystem'],
+                                                coords=data['StarPos'], date=data['timestamp'])
+                                try:
+                                    session.add(newsys)
+                                    transaction.commit()
+                                except DataError:
+                                    print("Failed to add a system! Invalid data passed")
+                                    transaction.abort()
+                        if data['event'] == 'Scan':
+                            bodyid = data['SystemAddress'] + (data['BodyID'] << 55)
+                            if 'AbsoluteMagnitude' in data:
+                                res = session.query(Star.id64).filter(Star.id64 == bodyid).scalar() or False
+                                if not res:
+                                    starcount = starcount + 1
+                                    newstar = Star(id64=bodyid, bodyId=data['BodyID'], name=data['BodyName'],
+                                                   age=data['Age_MY'], axialTilt=data['AxialTilt'],
+                                                   orbitalEccentricity=data['Eccentricity']
+                                                   if 'Eccentricity' in data else None,
+                                                   orbitalInclination=data['OrbitalInclination']
+                                                   if 'OrbitalInclination' in data else None,
+                                                   orbitalPeriod=data['OrbitalPeriod']
+                                                   if 'OrbitalPeriod' in data else None,
+                                                   parents=data['Parents']
+                                                   if 'Parents' in data else None,
+                                                   argOfPeriapsis=data['Periapsis']
+                                                   if 'Periapsis' in data else None,
+                                                   belts=data['Rings'] if 'Rings' in data else None,
+                                                   semiMajorAxis=data['SemiMajorAxis']
+                                                   if 'SemiMajorAxis' in data else None,
+                                                   systemName=data['StarSystem'],
+                                                   distanceToArrival=data['DistanceFromArrivalLS'],
+                                                   luminosity=data['Luminosity'], solarRadius=data['Radius'],
+                                                   rotationalPeriod=data['RotationPeriod'], type=data['StarType'],
+                                                   solarMasses=data['StellarMass'],
+                                                   subType=data['Subclass'] if 'Subclass' in data else None,
+                                                   surfaceTemperature=data['SurfaceTemperature'],
+                                                   isScoopable=True if data['StarType'] in __scoopable else False,
+                                                   isMainStar=True if data['BodyID'] == 0 else False,
+                                                   updateTime=data['timestamp'])
+                                    session.add(newstar)
+                                    print("Added new star.")
+                                    transaction.commit()
                 sys.stdout.flush()
 
         except zmq.ZMQError as e:
             print('ZMQSocketException: ' + str(e))
-            proxy.command("botserv", "Absolver", f"say #rattech [SAPI] EDDN error: "
+            proxy.command("botserv", "Absolver", f"say #rattech [\x0315SAPI\x03] EDDN error: "
                                                  f"Exiting due to exception: {str(e)}")
 
             sys.stdout.flush()
